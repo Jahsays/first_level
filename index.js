4915e1905b37ce0b55b535ebe3ba4ad998c89cc4
Chiaka Williams <chiaka394@gmail.com>
2021-10-09 12:00:33 +0100

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 function isUnique(string) {
          
-    for (let i = 0; i < string.length; i++) {
+    for (var i = 0; i < string.length; i++) {
         if (string.charAt(i) == string.charAt(i + 1)) {
             return false;
         }
@@ -22,11 +22,11 @@ var noOfChars = 256;
 
 function checkPermutation(string1, string2)
 {
-	let count1 = Array(noOfChars);
-	let count2 = Array(noOfChars);
+	var count1 = Array(noOfChars);
+	var count2 = Array(noOfChars);
 	count1.fill(0);
 	count2.fill(0);
-	let i;
+	var i;
 
 	for(i = 0;
 		i < string1.length && i < string2.length;
@@ -64,7 +64,7 @@ checkPermutation("carpet","carriage");  //false
 
 // 4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
 //  A palindrome is a word or phrase that is the same forwards and backwards. 
-// A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+// A permutation is a rearrangement of varters. The palindrome does not need to be limited to just dictionary words.
 //  EXAMPLE Input: Tact Coa Output: True (permutations: "taco cat", "atco eta", etc.)
 
 
@@ -83,31 +83,77 @@ checkPalindrome("kelly");    //false
 //  pale, bale -> true
 //   pale, bake -> false 
 
+
+	function oneAway(string1, string2){
+
+		var m = string1.length;
+    var n = string2.length;
+
+		if (Math.abs(m - n) > 1)
+			return false;
+
+			var count = 0;
+			var i = 0; 
+      var j = 0;
+
+		while (i < m && j < n)
+		{
+			if (string1[i] != string2[j])
+			{
+				if (count == 1)
+					return false;
+				if (m > n)
+					i++;
+				else if (m< n)
+					j++;
+				else
+				{
+					i++;
+					j++;
+				}
+
+				count++;
+			}
+			else
+			{
+				i++;
+				j++;
+			}
+		}
+		if (i < m || j < n)
+			count++;
+
+		return count == 1;
+	}
+	
+  oneAway("game","gam");    //true
+  oneAway("guile","guy");   //false
+
 // 6 String Compression: Implement a method to perform basic string compression using the counts of repeated
 //  characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string
 //  would not become smaller than the original string, your method should return the original string. 
-// You can assume the string has only uppercase and lowercase letters (a - z).
+// You can assume the string has only uppercase and lowercase varters (a - z).
 
 function stringCompression(char){
     var compressedString = ''
     const charArray = char.split('')
-    for (let i = 0; i < charArray.length; i++){
+    for (var i = 0; i < charArray.length; i++){
       var count = 1
-      var currentLetter = charArray[i]
+      var currentvarter = charArray[i]
       while (i < charArray.length - 1 && charArray[i] === charArray[i + 1]){
         count++
         i++
       }
       if (count === 1){
-        compressedString += currentLetter
+        compressedString += currentvarter
       } else {
-        compressedString += currentLetter + count
+        compressedString += currentvarter + count
       }
     }
     return compressedString
   }
 
-  stringCompression("trespass");   //'trespas2'
+  stringCompression("trespass");   //'trespastring2'
   stringCompression("transpire");  //'transpire'
 
 
